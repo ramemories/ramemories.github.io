0453e002585a5871e0e96be88b2a8b7fcdbe1cdb
Andrew Kim <107439239+i30101@users.noreply.github.com>
2023-12-07 22:57:49 -0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,101 @@
+const text = "random access memories"; // Replace with your desired text
+const charSpeed = 200;
+const spaceSpeed = 400;
+let index = 0;
+
+function typeWriter(callback) {
+  const typewriterDiv = document.getElementById("title-text");
+  if (index < text.length) {
+    typewriterDiv.innerHTML += text.charAt(index);
+    if (text.charAt(index) === " ") {
+      setTimeout(() => {
+        typeWriter(callback);
+      }, spaceSpeed);
+    } else {
+      setTimeout(() => {
+        typeWriter(callback);
+      }, charSpeed);
+    }
+    index++;
+  } else {
+    // When typing finishes, execute the callback
+    if (typeof callback === "function") {
+      callback();
+    }
+  }
+}
+
+typeWriter(() => {
+  // Callback function to execute after typing finishes
+  const mainContent = document.getElementById("main-content");
+  mainContent.style.display = "block";
+});
+
+// Rest of your existing code...
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+// ... (rest of your code remains unchanged)
+
+
+// Set canvas size to window size
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Characters used for the rain effect
+const characters = '01';
+
+// Create columns for the raindrops
+const columns = Math.floor(canvas.width / 10);
+
+// Array to store y positions of each raindrop
+const drops = [];
+
+// Initialize drops
+for (let i = 0; i < columns; i++) {
+    drops[i] = Math.floor(Math.random() * canvas.height);
+}
+
+// Function to draw rain
+function draw() {
+    // Semi-transparent black background to create fading effect
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // Set color and font for raindrops
+    ctx.fillStyle = '#00F'; // Green color for raindrops
+    ctx.font = '15px monospace'; // Font for raindrops
+
+    // Loop through each column
+    for (let i = 0; i < drops.length; i++) {
+        const text = characters[Math.floor(Math.random() * characters.length)];
+        ctx.fillText(text, i * 10, drops[i] * 10);
+
+        // Reset drop position if it goes off the screen
+        if (drops[i] * 10 > canvas.height && Math.random() > 0.975) {
+        drops[i] = 0;
+        }
+
+        // Move the drop down
+        drops[i]++;
+    }
+}
+
+// Function to animate the rain
+function animate() {
+    requestAnimationFrame(animate);
+    draw();
+}
+
+// Start the animation
+animate();
+
+// Adjust canvas size if window is resized
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    drops.length = Math.floor(canvas.width / 10);
+});
+
+
