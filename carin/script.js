dddb0bf884d9cce3d02675aa51ec660d51ac62fa
Andrew Kim <107439239+i30101@users.noreply.github.com>
2024-09-18 21:28:05 -0400

    carin

---
+++ carin/script.js
@@ -0,0 +1,14 @@
+/**
+ * Version: 1.0.0
+ * Since: 1 May 2024
+ * cairn
+ */
+
+// initialize bottom div
+const bottom = document.getElementById("bottom");
+
+// scroll bottom div into view
+bottom.scrollIntoView({
+    behavior: "smooth", 
+    block: "start"
+})
